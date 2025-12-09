1e4ec70635ced93008d3018f58165dac34f383a5
Danishwara Pracheta <danishwara81@gmail.com>
2025-12-09 11:41:04 +0800

    auth implementation

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
 // exceptions
 const ClientError = require('./exceptions/ClientError');
+
+// utils
+const TokenManager = require('./tokenize/TokenManager');
 
 // Machines
 const machines = require('./api/machines');
@@ -18,10 +22,22 @@ const SensorValidator = require('./validator/sensors/index');
 const diagnostics = require('./api/diagnostics');
 const DiagnosticsService = require('./services/postgres/DiagnosticsService');
 
+// Authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const AuthenticationValidator = require('./validator/authentications/index');
+
+// Users
+const users = require('./api/users');
+const UsersService = require('./services/postgres/UsersService');
+const UserValidator = require('./validator/users/index');
+
 const init = async () => {
     const machinesService = new MachinesService();
     const sensorsService = new SensorsService();
     const diagnosticsService = new DiagnosticsService();
+    const authenticationsService = new AuthenticationsService();
+    const usersService = new UsersService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -31,6 +47,28 @@ const init = async () => {
                 origin: ['*'],
             },
         },
+    });
+
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    server.auth.strategy('pm_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
 
     await server.register([
@@ -54,6 +92,22 @@ const init = async () => {
                 diagnosticsService,
                 sensorsService,
                 machinesService,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationValidator,
+            },
+        },
+        {
+            plugin: users,
+            options: {
+                service: usersService,
+                validator: UserValidator,
             },
         },
     ]);
